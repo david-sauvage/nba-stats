2bed18cfeebe40922f0e6ef3a0cf4a0cc2c81a5c
David Sauvage <davidsauvage8@gmail.com>
2018-10-07 20:46:44 +0200

    Put players stats and win/loss in vuex

--- src/store.js
+++ src/store.js
@@ -15,6 +15,10 @@ export default new Vuex.Store({
     },
     setTeams(state, teams) {
       state.teams = teams
+    }, 
+    enrichTeam(state, team) {
+      var idx = state.teams.findIndex((e) => e.teamId == team.teamId)
+      state.teams[idx] = team
     }
 
   },
@@ -37,22 +41,66 @@ export default new Vuex.Store({
         });
     },
 
-    LOAD_TEAMS: function ({ commit }) {
+    LOAD_TEAMS: function ({ dispatch, commit }) {
       axios.get('./data/teams.json')
         .then(response => {
             var teams = [];
             for (var teamFromNbaSite of response.data.league.standard) {
               if (teamFromNbaSite.isNBAFranchise) {
                   teams.push(teamFromNbaSite);
-                
               }
             }
             commit('setTeams', teams)
+            dispatch('LOAD_STATS', teams)
         })
         .catch(function(error) {
             console.log(error);
         });
+    },
+
+    LOAD_STATS: function ( {commit}, teams) {
+      for(let t of teams) {
+        axios.get('./data/'+t.teamId+'.json')
+        .then(response => {
+          var teamOverral = response.data.resultSets[0].rowSet
+          var playersFromNbaSite = response.data.resultSets[1].rowSet
+          var players = []
+          for (var p of playersFromNbaSite) {
+            var player = {
+              name:p[2],
+              min:p[7],
+              pts:p[27],
+              fgm:p[8],
+              fga:p[9],
+              fg3m:p[11],
+              fg3a:p[12],
+              ftm:p[14],
+              fta:p[15],
+              oreb:p[17],
+              dreb:p[18],
+              ast:p[20],
+              to:p[21],
+              stl:p[22],
+              blck:p[23],
+              pf:p[25],
+              plus_minus:p[28]
+            }
+            players.push(player)
+          }
+          var stats = {
+            players: players, 
+            win:teamOverral[0][5],
+            loss:teamOverral[0][6], 
+          }
+          t.stats = stats
+          commit('enrichTeam', t)
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+      }
     }
 
   }
 })
+
