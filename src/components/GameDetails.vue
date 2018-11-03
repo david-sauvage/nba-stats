62aeffcf1b4ce75ab5d6a35f38a81f9e9b74c33c
David Sauvage <davidsauvage8@gmail.com>
2018-11-03 11:50:17 +0100

    Externalize PlayersTable in order to use it in other features later

--- src/components/GameDetails.vue
+++ src/components/GameDetails.vue
@@ -1,49 +1,19 @@
 <template>
     <v-container  fluid grid-list-xl >
         <Game :awayTeamId="$route.params.awayTeamId" :homeTeamId="$route.params.homeTeamId"/>
-
-        <v-data-table
-            :headers="headers"
-            :items="enrichedPlayers"
-            hide-actions
-            class="elevation-1">
-
-            <template slot="items" slot-scope="props">
-                <td>{{ props.item.name }}</td>
-                <td class="text-xs-right">{{ props.item.min }}</td>
-                <td class="text-xs-right">{{ props.item.pts }}</td>
-                <td class="text-xs-right">{{ props.item.reb }}</td>
-                <td class="text-xs-right">{{ props.item.ast }}</td>
-                <td class="text-xs-right">{{ props.item.blck }}</td>
-                <td class="text-xs-right">{{ props.item.stl }}</td>
-                <td class="text-xs-right">{{ props.item.ttfl }}</td>
-                <td class="text-xs-right">{{ props.item.jde }}</td>
-            </template>
-        </v-data-table>
+        <PlayersTable :players="players"/>
     </v-container>
 </template>
 
 <script>
 import Game from './Game.vue'
+import PlayersTable from './PlayersTable.vue'
 
 export default {
     name: 'GameDetails',
     components: {
-        Game
+        Game, PlayersTable
     },
-    data: () => ({
-      headers: [
-          { text: 'Name', align: 'left', value: 'name' },
-          { text: 'Minutes', align: 'right', value: 'min' },
-          { text: 'Points', align: 'right', value: 'pts' },
-          { text: 'Rebounds', align: 'right', value: 'reb' }, //TODO sum dreb and oreb
-          { text: 'Assists', align: 'right', value: 'ast' },
-          { text: 'Blocks', align: 'right', value: 'blck' },
-          { text: 'Steals', align: 'right', value: 'stl' },
-          { text: 'TTFL', align: 'right', value: 'ttfl' },
-          { text: 'JDE', align: 'right', value: 'jde' }
-        ],
-    }),
     computed: {
         players () {
            let awayPlayers = this.$store.state.teams.find((team) => {
@@ -56,46 +26,7 @@ export default {
 
             return [...awayPlayers, ...homePlayers]
         },
-        enrichedPlayers() {
-            return this.players.map( p => {
-                return {...p, 
-                        reb:p.oreb+p.dreb,
-                        ttfl:this.calculateTTFL(p),
-                        jde:this.calculateJDE(p)}
-            })
-        }
-    },
-    methods: {
-        calculateTTFL(player) {
-            const {pts, oreb, dreb, ast, stl, blck, fg3a, fg3m, fga, fgm, fta, ftm, to} = player
-            let calcul = pts + oreb + dreb + ast + stl + blck + fg3m + fgm + ftm 
-                            - (to + (fg3a - fg3m) + (fga - fgm) + (fta - ftm))
-            return calcul.toFixed(1)
-        },
-        calculateJDE(player) {
-            const {pts, oreb, dreb, ast, stl, blck, fg3a, fg3m, fga, fgm, fta, ftm, to, plus_minus, pf} = player
-            let calcul = plus_minus * 0.15
-            calcul += (fgm - fg3m) * 2
-            calcul += (fga - fgm - fg3m) * -0.5
-            calcul += fg3m * 3
-            calcul += (fg3a - fg3m) * -0.5
-            calcul += ftm 
-            calcul += (fta - ftm) * -0.5
-            calcul += dreb *0.75
-            calcul += oreb
-            calcul += ast
-            calcul += stl * 2
-            calcul += blck * 2
-            calcul += to * -0.75
-            calcul += pf * -0.5
-            return calcul.toFixed(2)
-        }
-
-    },
-
-   
-   
-  
+    }
 }
 </script>
 
---
+++ src/components/PlayersTable.vue
@@ -0,0 +1,83 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="enrichedPlayers"
+        hide-actions
+        class="elevation-1">
+
+        <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.min }}</td>
+            <td class="text-xs-right">{{ props.item.pts }}</td>
+            <td class="text-xs-right">{{ props.item.reb }}</td>
+            <td class="text-xs-right">{{ props.item.ast }}</td>
+            <td class="text-xs-right">{{ props.item.blck }}</td>
+            <td class="text-xs-right">{{ props.item.stl }}</td>
+            <td class="text-xs-right">{{ props.item.ttfl }}</td>
+            <td class="text-xs-right">{{ props.item.jde }}</td>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+
+export default {
+    name: 'PlayersTable',
+    props: ['players'],
+    data: () => ({
+      headers: [
+          { text: 'Name', align: 'left', value: 'name' },
+          { text: 'Minutes', align: 'right', value: 'min' },
+          { text: 'Points', align: 'right', value: 'pts' },
+          { text: 'Rebounds', align: 'right', value: 'reb' }, 
+          { text: 'Assists', align: 'right', value: 'ast' },
+          { text: 'Blocks', align: 'right', value: 'blck' },
+          { text: 'Steals', align: 'right', value: 'stl' },
+          { text: 'TTFL', align: 'right', value: 'ttfl' },
+          { text: 'JDE', align: 'right', value: 'jde' }
+        ],
+    }),
+    computed: {
+        enrichedPlayers() {
+            return this.players.map( p => {
+                return {...p, 
+                        reb:p.oreb+p.dreb,
+                        ttfl:this.calculateTTFL(p),
+                        jde:this.calculateJDE(p)}
+            })
+        }
+    },
+    methods: {
+        calculateTTFL(player) {
+            const {pts, oreb, dreb, ast, stl, blck, fg3a, fg3m, fga, fgm, fta, ftm, to} = player
+            let calcul = pts + oreb + dreb + ast + stl + blck + fg3m + fgm + ftm 
+                            - (to + (fg3a - fg3m) + (fga - fgm) + (fta - ftm))
+            return calcul.toFixed(1)
+        },
+        calculateJDE(player) {
+            const {pts, oreb, dreb, ast, stl, blck, fg3a, fg3m, fga, fgm, fta, ftm, to, plus_minus, pf} = player
+            let calcul = plus_minus * 0.15
+            calcul += (fgm - fg3m) * 2
+            calcul += (fga - fgm - fg3m) * -0.5
+            calcul += fg3m * 3
+            calcul += (fg3a - fg3m) * -0.5
+            calcul += ftm 
+            calcul += (fta - ftm) * -0.5
+            calcul += dreb *0.75
+            calcul += oreb
+            calcul += ast
+            calcul += stl * 2
+            calcul += blck * 2
+            calcul += to * -0.75
+            calcul += pf * -0.5
+            return calcul.toFixed(2)
+        }
+
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
